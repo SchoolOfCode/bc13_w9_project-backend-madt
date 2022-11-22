796ee98a56edea611a8c9e3d98c1cec1dfa3f551
Tirsog <tirsog@gmail.com>
2022-11-22 10:34:04 +0000

    initial

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+/*
+const PORT = process.env.PORT;
+*/
+const PORT = 3000;
+const morgan = require("morgan")
+
+
+const postRouter = require("./routes/post.js")
+const customerRouter = require("./routes/customer.js")
+
+
+// Parse the request JSON Body - This is an example of Middleware.
+app.use(express.json());
+
+// Using Morgan
+// Morgan shows a report in the console. "GET / 200 1.693 ms - 70"
+app.use(morgan("dev"))
+
+// Using Router for each endpoint
+
+app.use("/api/post", postRouter);
+app.use("/api/customer", customerRouter);
+
+
+// Listening Port
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
---
+++ routes/post.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+// Write your router code here!
+
+module.exports = router;
+
